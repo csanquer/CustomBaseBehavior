6290254f1a3def0c1be0557fcc8c8714a4876e7a
Charles SANQUER <charles.sanquer@spyrit.net>
2012-05-21 14:14:19 +0200

    fix indentation

--- src/CustomBaseBehavior.php
+++ src/CustomBaseBehavior.php
@@ -8,45 +8,46 @@
 class CustomBaseBehavior extends Behavior
 {
     // default parameters value
-	protected $parameters = array(
-		'base_object' => 'BaseObject',
+    protected $parameters = array(
+        'base_object' => 'BaseObject',
         'base_peer' => '',
         'base_query' => 'ModelCriteria',
-	);
-    
+    );
+
     protected function cleanfullyQualifiedClassName($fullyQualifiedClassName)
     {
         return trim(str_replace(array('.', '/'), '\\', $fullyQualifiedClassName), '\\');
     }
-    
+
     protected function getClassName($fullyQualifiedClassName)
     {
-		if (($pos = strrpos($fullyQualifiedClassName, '\\')) !== false) 
+        if (($pos = strrpos($fullyQualifiedClassName, '\\')) !== false)
         {
             $className = substr($fullyQualifiedClassName, $pos + 1);
             $namespace = substr($fullyQualifiedClassName, 0, $pos);
             return $className;
-		} 
+        }
         return $fullyQualifiedClassName;
     }
-    
+
     public function parentClass($builder)
-	{
-        switch (get_class($builder)) {
-			case 'PHP5ObjectBuilder':
+    {
+        switch (get_class($builder))
+        {
+            case 'PHP5ObjectBuilder':
                 $class = $this->getParameter('base_object');
-				break;
-			case 'QueryBuilder':
+                break;
+            case 'QueryBuilder':
                 $class = $this->getParameter('base_query');
-				break;
-			case 'PHP5PeerBuilder':
+                break;
+            case 'PHP5PeerBuilder':
                 $class = $this->getParameter('base_peer');
-				break;
-			default:
-				$class = null;
-				break;
-		}
-        
+                break;
+            default:
+                $class = null;
+                break;
+        }
+
         if (!empty($class))
         {
             $class = $this->cleanfullyQualifiedClassName($class);
@@ -54,6 +55,5 @@ class CustomBaseBehavior extends Behavior
             return $this->getClassName($class);
         }
         return null;
-	}
+    }
 }
-
