85367afce7f29a21b6cee27b3016f9be053a0b4f
Charles SANQUER <charles.sanquer@spyrit.net>
2012-05-21 12:59:06 +0200

    initial import

--- README.md
+++ README.md
@@ -2,3 +2,39 @@ CustomBaseBehavior
 ==================
 
 Propel Behavior to customize generated Base classes extending
+
+Requirements
+------------
+
+This behavior requires Propel >=1.6.0
+
+Installation
+------------
+
+Copy the behavior to a specific path and then register the behavior class by adding the following to the bottom of the `build.properties` file in you project folder:
+
+```ini
+# check that you have behaviors enabled
+propel.builder.addBehaviors = true
+
+# and add the custom behavior
+propel.behavior.custom_base.class = path.to.CustomBehavior
+```
+
+Enable the behavior in your schema.xml:
+
+```xml
+<table name="book">
+  <column name="id" required="true" primaryKey="true" autoIncrement="true" type="INTEGER" />
+  <column name="title" type="VARCHAR" required="true" />
+
+  <behavior name="custom_base">
+    <parameter name="base_object" value="MyCustomBaseObject" /> <!-- default = "BaseObject" -->
+    <parameter name="base_peer" value="MyCustomBasePeer" />     <!-- default = "" -->
+    <parameter name="base_Query" value="MyCustomBaseQuery" />   <!-- default = "ModelCriteria" -->
+  </behavior>
+</table>
+
+Base classes can use namespaces ( with \, / or dot character).
+
+Your custom Base Object class should extend Propel BaseObject and Your custom Base Query class should extend Propel ModelCriteria
---
+++ src/CustomBaseBehavior.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+ * CustomBaseBehavior
+ *
+ * @author Charles SANQUER - <charles.sanquer@gmail.com>
+ */
+class CustomBaseBehavior extends Behavior
+{
+    // default parameters value
+	protected $parameters = array(
+		'base_object' => 'BaseObject',
+        'base_peer' => '',
+        'base_query' => 'ModelCriteria',
+	);
+    
+    protected function cleanfullyQualifiedClassName($fullyQualifiedClassName)
+    {
+        return trim(str_replace(array('.', '/'), '\\', $fullyQualifiedClassName), '\\');
+    }
+    
+    protected function getClassName($fullyQualifiedClassName)
+    {
+		if (($pos = strrpos($fullyQualifiedClassName, '\\')) !== false) 
+        {
+            $className = substr($fullyQualifiedClassName, $pos + 1);
+            $namespace = substr($fullyQualifiedClassName, 0, $pos);
+            return $className;
+		} 
+        return $fullyQualifiedClassName;
+    }
+    
+    public function parentClass($builder)
+	{
+        switch (get_class($builder)) {
+			case 'PHP5ObjectBuilder':
+                $class = $this->getParameter('base_object');
+				break;
+			case 'QueryBuilder':
+                $class = $this->getParameter('base_query');
+				break;
+			case 'PHP5PeerBuilder':
+                $class = $this->getParameter('base_peer');
+				break;
+			default:
+				$class = null;
+				break;
+		}
+        
+        if (!empty($class))
+        {
+            $class = $this->cleanfullyQualifiedClassName($class);
+            $builder->declareClass($class);
+            return $this->getClassName($class);
+        }
+        return null;
+	}
+}
+
